d2c69a78238b71de8836e313ac12029431798b89
Atman Dhol <atmandhol@hotmail.com>
2024-02-08 13:50:40 -0500

    feat: Updated to run into multiple namespaces and work with rc.9

--- scripts/create-workloads.sh
+++ scripts/create-workloads.sh
@@ -6,4 +6,6 @@ export KUBECONFIG=../kubeconfig.yaml
 
 # Create workloads
 cd workloads
-tanzu workload apply -y
+tanzu workload apply -n default -y
+tanzu workload apply -f workload-dev.yaml -n dev -y
+tanzu workload apply -f workload-supplychain.yaml -n supplychain -y
--- scripts/install-tap.sh
+++ scripts/install-tap.sh
@@ -10,7 +10,15 @@ kapp deploy -a secretgen-controller -n kube-system -f https://github.com/vmware-
 
 # Install TAP and Supplychain Packages
 kubectl create ns tap-install || true
+kubectl create ns dev || true
+kubectl create ns supplychain || true
 
 ytt -f cluster --data-values-file values.yaml | kapp deploy -a tap-installation -c -f- --yes
 
 kubectl label ns default apps.tanzu.vmware.com/tap-ns=""
+kubectl label ns dev apps.tanzu.vmware.com/tap-ns=""
+kubectl label ns supplychain apps.tanzu.vmware.com/tap-ns=""
+
+ytt -f namespace --data-values-file values.yaml | kapp deploy -a default-ns-setup -n default -c -f- --yes
+ytt -f namespace --data-values-file values.yaml | kapp deploy -a dev-ns-setup -n dev -c -f- --yes
+ytt -f namespace --data-values-file values.yaml | kapp deploy -a supplychain-ns-setup -n supplychain -c -f- --yes
